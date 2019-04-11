4996517f8d4108528e0a61b02047787a1662fe16
Theo Turner <10600558+theoturner@users.noreply.github.com>
2019-04-11 17:34:59 +0100

    Run Fetch container in background

--- fetch_ai.sh
+++ fetch_ai.sh
@@ -3,4 +3,4 @@
 git clone https://github.com/fetchai/oef-core.git
 cd oef-core
 ./oef-core-image/scripts/docker-build-img.sh
-./oef-core-image/scripts/docker-run.sh -p 3333:3333 -- &
+./oef-core-image/scripts/docker-run.sh -d -p 3333:3333 -- &
