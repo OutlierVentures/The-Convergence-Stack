56ce99c816fdca65cc1535be8d1b7dbd3434ee1f
Theo Turner <theo_turner@outlook.com>
2019-08-14 17:11:10 +0100

    Add stop nodes

---
+++ stop_nodes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ongreen='\033[42m'
+onyellow='\033[43m'
+endcolor='\033[0m'
+
+echo -e "${onyellow}Stopping nodes...$endcolor"
+
+docker stop $(docker ps -a | grep oef-search | awk '{print $1}') &> /dev/null
+kill -INT $(ps | grep oef-mt-core | awk '{print $1}') &> /dev/null
+kill -INT $(ps | grep constellation | awk '{print $1}') &> /dev/null
+
+docker stop $(docker ps -a | grep ocean_ | awk '{print $1}') &> /dev/null
+
+echo -e "${ongreen}Nodes stopped.$endcolor"
